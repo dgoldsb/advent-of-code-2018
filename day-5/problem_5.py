5e6aaca97253364780d1bd41437f9993585728b2
Dylan Goldsborough <dgoldsb@live.nl>
2018-12-07 00:34:24 +0100

    Outline

--- day-5/problem_5.py
+++ day-5/problem_5.py
@@ -9,49 +9,25 @@ What is the size of the largest area that isn't infinite?
 """
 
 
-
-def angle_clockwise(A, B):
-
-
-def convex_hull(input_points):
-    # Pick leftmost index.
-    left_index = 0
-    left_value = input_points[0][0]
-    for i, value in enumerate(input_points):
-        if input_points[i][0] < left_value:
-            left_index = i
-            left_value = input_points[i][0]
-            input_points[i][0]
-
-    # Start the hull.
-    last_line = ((input_points[left_index][0], input_points[left_index][0]),
-                  input_points[left_index])
-    hull = [left_index]
-    while (hull[0] != hull[-1] or len(hull) == 1) and len(input_points) != 1:
-        smallest_angle = 360
-        smallest_id = None
-
-        for i, value in enumerate(input_points):
-            if i == hull[-1]:
-                continue
-            angle = angle_clockwise(last_line, ((input_points[hull[-1]], input_points[i])))
-            if angle < smallest_angle:
-                smallest_angle = angle
-                smallest_id = id
-
-        last_line = (input_points[hull[-1]], input_points[smallest_Id])
-        hull.append(smallest_id)
-
-    return hull
-
-
 if __name__ == '__main__':
     # Read input.
     with open('input', 'r') as file:
         points = []
         for line in file:
-            points.append(tuple([int(x) for x in line.replace('\n', '').split(',')]))
+            points.append([int(x) for x in line.replace('\n', '').split(',')])
 
-    # Exclude the convex hull.
+    # Add a point ID to deal with identical points later on.
+    for index, _ in enumerate(points):
+        points[index] = points[index] + [index]
 
-    print(convex_hull(points))
+    # Create a box with a safeish boundary.
+
+    # Shift the coordinates to be nice and in the box.
+
+    # Floodfill the box.
+
+    # Create a list of candidates that do not touch the edge.
+
+    # Iterate over candidates and find the largest area.
+
+    print(f'Answer 1 is {largest_area} squares.')
