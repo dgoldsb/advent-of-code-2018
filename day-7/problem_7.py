b6aa0441c4ef456274b8274be258d170567e7883
Dylan Goldsborough <dgoldsb@live.nl>
2018-12-17 20:34:03 +0100

    Easy peazy lemon squeezy

--- day-7/problem_7.py
+++ day-7/problem_7.py
@@ -28,13 +28,31 @@ In this example, each node of the tree is also marked with an underline starting
 The first check done on the license file is to simply add up all of the metadata entries. In this example, that sum is 1+1+2+10+11+12+2+99=138.
 
 What is the sum of all metadata entries?
+
+The second check is slightly more complicated: you need to find the value of the root node (A in the example above).
+
+The value of a node depends on whether it has child nodes.
+
+If a node has no child nodes, its value is the sum of its metadata entries. So, the value of node B is 10+11+12=33, and the value of node D is 99.
+
+However, if a node does have child nodes, the metadata entries become indexes which refer to those child nodes. A metadata entry of 1 refers to the first child node, 2 to the second, 3 to the third, and so on. The value of this node is the sum of the values of the child nodes referenced by the metadata entries. If a referenced child node does not exist, that reference is skipped. A child node can be referenced multiple time and counts each time it is referenced. A metadata entry of 0 does not refer to any child node.
+
+For example, again using the above nodes:
+
+    Node C has one metadata entry, 2. Because node C has only one child node, 2 references a child node which does not exist, and so the value of node C is 0.
+    Node A has three metadata entries: 1, 1, and 2. The 1 references node A's first child node, B, and the 2 references node A's second child node, C. Because node B has a value of 33 and node C has a value of 0, the value of node A is 33+33+0=66.
+
+So, in this example, the value of the root node is 66.
+
+What is the value of the root node?
 """
 
 class Node:
     def __init__(self, a, b):
         self._child_count = a
         self._meta_count = b
-        self.metadata = None
+        self.metadata = []
+        self.children = []
 
 
 def recursive_decompose(sequence):
@@ -46,6 +64,7 @@ def recursive_decompose(sequence):
     if nodes[0]._child_count > 0:
         for _ in range(nodes[0]._child_count):
             children, remainder = recursive_decompose(sequence)
+            nodes[0].children.append(children[0])
             nodes = nodes + children
             sequence = remainder
 
@@ -54,6 +73,17 @@ def recursive_decompose(sequence):
         sequence = sequence[nodes[0]._meta_count:]
 
     return nodes, sequence
+
+
+def recursive_value(node):
+    if len(node.children) == 0:
+        return sum(node.metadata)
+
+    total = 0
+    for index in node.metadata:
+        if index <= len(node.children):
+            total += recursive_value(node.children[index - 1])
+    return total
 
 
 if __name__ == '__main__':
@@ -70,3 +100,6 @@ if __name__ == '__main__':
         for meta in node.metadata:
             total += meta
     print(f'Answer 1 is {total}.')
+
+    # Answer 2.
+    print(f'Answer 2 is {recursive_value(nodes[0])}.')
