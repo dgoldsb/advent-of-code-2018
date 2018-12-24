8a2d9e568593eec52929aa99a8f591a32419df00
Dylan Goldsborough <dgoldsb@live.nl>
2018-12-24 18:37:46 +0100

    Turns out my regex was wrong, hahaha..

--- day-23/problem_23.py
+++ day-23/problem_23.py
@@ -25,12 +25,23 @@ The defending group only loses whole units from damage; damage is always dealt i
 After the fight is over, if both armies still contain units, a new fight begins; combat only ends once one army has lost all of its units.
 
 You scan the reindeer's condition (your puzzle input); the white-bearded man looks nervous. As it stands now, how many units would the winning army have?
+
+--- Part Two ---
+
+Things aren't looking good for the reindeer. The man asks whether more milk and cookies would help you think.
+
+If only you could give the reindeer's immune system a boost, you might be able to change the outcome of the combat.
+
+A boost is an integer increase in immune system units' attack damage.
+
+How many units does the immune system have left after getting the smallest boost it needs to win?
 """
 
+from copy import deepcopy
 import re
 
 
-DEBUG = True
+DEBUG = False
 
 
 class Group:
@@ -120,6 +131,75 @@ class Army:
         return target_damage, target_army, target_key
 
 
+def battle(armies, boost=0):
+    # Apply the boost.
+    for group in armies['Immune System'].groups.values():
+        group.dmg += boost
+
+    last_remaining_count = 0
+    while False not in [x.alive for x in armies.values()]:
+        # Find targets.
+        targets = []
+
+        for army in list(armies.values())[::-1]:
+            # Get all groups : army combinations.
+            exclude_defend = []
+            combinations = []
+            for gk in army.groups.keys():
+                combinations.append([army, gk])
+
+            combinations.sort(key=lambda row: (-row[0].groups[row[1]].effective_power, -row[0].groups[row[
+                1]].initiative))
+
+            for combination in combinations:
+                damage, target_army, target_group_key = combination[0].find_target(combination[1], armies,
+                                                                                   exclude_defend)
+                if target_army is None:
+                    continue
+
+                target = dict()
+                target['target_army'] = target_army
+                target['target_group_key'] = target_group_key
+                target['attack_army'] = combination[0]
+                target['attack_group_key'] = combination[1]
+
+                targets.append(target)
+                exclude_defend.append(target_army.groups[target_group_key])
+
+        targets.sort(key=lambda d: -d['attack_army'].groups[d['attack_group_key']].initiative)
+
+        for target in targets:
+            try:
+                target['target_army'].groups[target['target_group_key']].defend(target['attack_army'],
+                                                                                target['attack_group_key'],
+                                                                                target['target_group_key'])
+            except KeyError:
+                if DEBUG:
+                    print(f'Deleted group {target["attack_group_key"]} from '
+                          f'{target["attack_army"].name} cannot do anything.')
+
+            if target['target_army'].groups[target['target_group_key']].count == 0:
+                del target['target_army'].groups[target['target_group_key']]
+
+                if DEBUG:
+                    print(f'Deleted group {target["attack_group_key"]} from {target["attack_army"].name}.')
+
+        if DEBUG:
+            print(f'Battle status: {armies["Immune System"].alive_count} and {armies["Infection"].alive_count}.')
+
+        remaining_count = sum([a.alive_count for a in armies.values()])
+
+        if remaining_count == last_remaining_count:
+            return None, None
+        else:
+            last_remaining_count = remaining_count
+
+    if armies['Immune System'].alive_count > 0:
+        return True, remaining_count
+    else:
+        return False, remaining_count
+
+
 if __name__ == '__main__':
     # Read input.
     with open('input', 'r') as file:
@@ -155,46 +235,35 @@ if __name__ == '__main__':
 
                 armies[current_army].add_group(Group(count, hp, initiative, dmg, dmg_type, weak, immune))
 
-    while False not in [x.alive for x in armies.values()]:
-        # Find targets.
-        targets = []
+            match = re.match('([0-9]+) units each with ([0-9]+) hit points with an attack that does (['
+                             '0-9]+) ([a-z]+) damage at initiative ([0-9]+)', line)
+            if match:
+                group_count += 1
+                count = int(match.group(1))
+                hp = int(match.group(2))
+                dmg = int(match.group(3))
+                dmg_type = match.group(4)
+                initiative = int(match.group(5))
+                weak = []
+                immune = []
 
-        # Get all groups : army combinations.
-        exclude_defend = []
+                armies[current_army].add_group(Group(count, hp, initiative, dmg, dmg_type, weak, immune))
 
-        for army in list(armies.values())[::-1]:
-            combinations = []
-            for gk in army.groups.keys():
-                combinations.append([army, gk])
-
-            combinations.sort(key=lambda row: (-row[0].groups[row[1]].effective_power, -row[0].groups[row[
-                1]].initiative))
-
-            for combination in combinations:
-                dmg, ta, tgk = combination[0].find_target(combination[1], armies, exclude_defend)
-                if ta is None:
-                    continue
-                target = [combination[0].groups[combination[1]].initiative, combination[0], combination[1], ta, tgk]
-                targets.append(target)
-                exclude_defend.append(ta.groups[tgk])
-
-        targets.sort(key=lambda row: -row[0])
-
-        for target in targets:
-            try:
-                target[3].groups[target[4]].defend(target[1], target[2], target[4])
-            except KeyError:
-                if not DEBUG:
-                    print(f'Deleted group {target[2]} from {target[1].name} cannot do anything.')
-
-            if target[3].groups[target[4]].count == 0:
-                del target[3].groups[target[4]]
-
-                if not DEBUG:
-                    print(f'Deleted group {target[4]} from {target[3].name}.')
-
-        if not DEBUG:
-            print(f'Battle status: {armies["Immune System"].alive_count} and {armies["Infection"].alive_count}.')
-
-    remaining_count = sum([a.alive_count for a in armies.values()])
+    _, remaining_count = battle(deepcopy(armies))
     print(f'Answer 1 is {remaining_count}.')
+
+    # Now loop over increasing integers and see if this does not take too long.
+    boost = 0
+
+    last_won = False
+    last_remaining_count = 0
+    while not last_won:
+        boost += 1
+        last_won, last_remaining_count = battle(deepcopy(armies), boost)
+        if last_won is None:
+            print('They tied!')
+        elif not last_won:
+            print(f'The immune system lost with {last_remaining_count}')
+
+    print(f'Answer 2 is {last_remaining_count}.')
+
