2f281fffb6efe00738fe162c53039e5abe99aebf
Dylan Goldsborough <dgoldsb@live.nl>
2018-12-25 02:11:36 +0100

    Leaving this for now

--- day-21/problem_21.py
+++ day-21/problem_21.py
@@ -73,17 +73,83 @@ Finally, once you reach the target, you need the torch equipped before you can f
 This is tied with other routes as the fastest way to reach the target: 45 minutes. In it, 21 minutes are spent switching tools (three times, seven minutes each) and the remaining 24 minutes are spent moving.
 
 What is the fewest number of minutes you can take to reach the target?
-
 """
 
+from collections import defaultdict
+import heapq
+import numpy as np
 import re
 
-import numpy as np
+
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.weights = {}
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.weights[(from_node, to_node)] = distance
+
+    def neighbors(self, current):
+        return self.edges[current]
+
+    def cost(self, current, next):
+        return self.weights[(current, next)]
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+def heuristic(a, b):
+    (x1, y1, z1) = a
+    (x2, y2, z2) = b
+    return abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)
+
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
 
 
 if __name__ == '__main__':
     # Read input.
-    with open('input', 'r') as file:
+    with open('example', 'r') as file:
         body = file.read()
         depth = int(re.search('depth: ([0-9]+)', body).group(1))
         match = re.search('target: ([0-9]+),([0-9]+)', body)
@@ -94,7 +160,7 @@ if __name__ == '__main__':
         return (a + depth) % 20183
 
     # Create the grid.
-    geo_index = np.empty(shape=(xt+1,yt+1))
+    geo_index = np.empty(shape=(xt+1, yt+1))
     total_risk_level = 0
 
     for x in range(xt+1):
@@ -112,3 +178,54 @@ if __name__ == '__main__':
             total_risk_level += int(geo_to_ero(geo_index[x][y]) % 3)
 
     print(f'Answer 1 is {total_risk_level}')
+
+    # We need a bigger grid, let's make a safety margin.
+    big_grid = np.empty(shape=(xt+50, yt+50))
+    for x in range(xt+50):
+        for y in range(yt+50):
+            if (x == 0 and y == 0) or (x == xt and y == yt):
+                big_grid[x][y] = 0
+            elif y == 0:
+                big_grid[x][y] = x * 16807
+            elif x == 0:
+                big_grid[x][y] = y * 48271
+            else:
+                big_grid[x][y] = geo_to_ero(big_grid[x-1][y]) * geo_to_ero(big_grid[x][y-1])
+
+    def geo_to_type(a):
+        return geo_to_ero(a) % 3
+
+    # Construct the graph.
+    graph = Graph()
+    for x in range(xt+50):
+        for y in range(yt+50):
+            terrain = geo_to_type(big_grid[x][y])
+
+            for state in [0, 1, 2]:  # 0 none, 1 climbing gear, 2 flashlight
+                if (terrain != state) or terrain == 0:
+                    # Create a node.
+                    node = (x, y, state)
+                    graph.add_node(node)
+
+                    # Now to create some fucking edges.
+                    for xdt in [-1, 1]:
+                        if x + xdt == -1 or x + xdt == xt + 50:
+                            continue
+                        for ydt in [-1, 1]:
+                            if y + ydt == -1 or y + ydt == yt + 50:
+                                continue
+
+                            terrain_next = geo_to_type(big_grid[x + xdt][y + ydt])
+                            if (terrain_next != state) or terrain_next == 0:
+                                node_next = (x + xdt, y + ydt, state)
+                                graph.add_node(node_next)
+                                graph.add_edge(node, node_next, 1)
+
+                    # Also we can switch our fucking gear.
+                    for state_next in [0, 1, 2]:
+                        if ((terrain != state_next) or terrain == 0) and state != state_next:
+                            node_next = (x, y, state_next)
+                            graph.add_edge(node, node_next, 7)
+
+    start, cost_so_far = a_star_search(graph, (0, 0, 1), (xt, yt, 1))
+    print(f'Answer 2 is {cost_so_far[(xt, yt, 1)]}.')
